16a08d5a8822008a1e97dde98347b8bb364a4cc1
marek_przypadlo <marek@przypadlo.pl>
2020-03-28 23:49:04 +0100

    Return after invalid login

--- src/web_socket_handler.js
+++ src/web_socket_handler.js
@@ -3,7 +3,7 @@ class UserConnection
     /**
      * @param {id} Number
      * @param {String} username
-     * @param {WebScoket} connection
+     * @param {WebSocket} connection
      */
     constructor(id, username, connection) {
         this.id = id; 
@@ -39,6 +39,31 @@ class WebSocketHandler
         );
 
         webSocket.on('message', this.handleMessage(user));
+        webSocket.on('close', this.handleClose(user));
+    }
+
+    /**
+     * 
+     * @param {UserConnection} user 
+     */
+    handleClose(user) {
+        /**
+         * @param {WebSocket} connection
+         */
+        return (conection) => {
+            if (!user.username) {
+                return;
+            }
+
+            this.namedConnections = this.namedConnections.filter((u) => {
+                return u.id != user.id;
+            })
+            
+            this.nofityAll({
+                type: "PLAYER-LEFT",
+                data: this.toUser(user)
+            });
+        }
     }
 
     /**
@@ -89,6 +114,8 @@ class WebSocketHandler
                 type: "INVALID-USERNAME",
                 message: "Username cannot be null"
             })
+
+            return;
         }
 
         if (issuer.username) {
@@ -96,23 +123,40 @@ class WebSocketHandler
                 type: "INVALID-LOGIN", 
                 message: "User already logged in"
             });
+
+            return;
         }
 
         issuer.username = command.username;
-        this.namedConnections.push(issuer);
 
         issuer.receive({
             type: "CORRECT-LOGIN",
-            playerId: issuer.id
+            playerId: issuer.id, 
+            others: this.namedConnections.map(this.toUser)
         });
+
+        this.namedConnections.push(issuer);
 
         this.nofityAll({
             type: "PLAYER-JOINED",
             data: {
                 userName: issuer.username,
+                id: issuer.id,
                 hp: 100
             }
         });
+    }
+
+    /**
+     * 
+     * @param {UserConnection} connection 
+     */
+    toUser(connection) {
+        return {
+            userName: connection.username,
+            id: connection.id,
+            hp: 100
+        }
     }
 
     nofityAll(message) {
