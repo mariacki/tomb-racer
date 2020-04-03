285246797707d8dbc544f1ec633de07119cc5932
mprzypadlo <mprzypadlo@github.com>
2020-04-04 00:54:36 +0200

    Mild refactoring

---
+++ test/movement_test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const ustils = require('./utils');
+const game = require('../src/game');
+
+describe('Moving', () => {
+    it('should be allowed only after game started', () => {
+        
+    })
+})
